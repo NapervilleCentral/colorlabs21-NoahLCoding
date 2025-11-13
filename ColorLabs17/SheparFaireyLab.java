40e3c417dc9678239e4bd129b39edd53350d9c7e
NoahLCoding <noahnojus2008@gmail.com>
2025-11-13 11:49:52 -0600

    Shepard fairy

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -46,25 +46,114 @@ public class SheparFaireyLab
             }
          volleyball.explore();
          System.out.println("Min: " + min + " Max: " + max);
-         int group1 = (int) (255/4);
-         int group2 = (int) ((2*255)/4);
-         int group3 = (int) ((3*255)/4);
+         /*
+         int group1 = (int) (255/5);
+         int group2 = (int) ((2*255)/5);
+         int group3 = (int) ((3*255)/5);
+         int group4 = (int) ((4*255)/5);
          for (Pixel pixel: vPixels){
-             
+             int gray = pixel.getRed();
+             Color newColor;
+             if (gray<group1){
+                red = 20;
+                green = 20;
+                blue = 139;
+                }
+             else if(gray<group2){
+                red = 180;
+                green = 0;
+                blue = 0;
+                }
+             else if(gray<group3){
+                red = 173;
+                green = 216;
+                blue = 230;                
+                }
+             else if (gray<group4){
+                red = 255;
+                green = 223;
+                blue = 178;
+                }
+             else{
+                 red = 255;
+                 green = 182;
+                 blue = 193;
+                }
+             newColor = new Color(red, green, blue);
+             pixel.setColor(newColor);
             }
-         /**
-          * method 1 change
-          * 
-          */
-         
-         /**
-          * method 2 change
-          * 
-          */
-         
-         /**
-          * custom color palette
-          */
+            volleyball.explore();
+            volleyball.write("images/sf1.jpg");
+         /*
+         int range = max - min;
+         int group1 = (int) (range/4);
+         int group2 = (int) ((2*range)/4);
+         int group3 = (int) ((3*range)/4);
+         for (Pixel pixel: vPixels){
+            int gray = pixel.getRed();
+             Color newColor;
+             if (gray<group1){
+                red = 20;
+                green = 20;
+                blue = 139;
+                }
+             else if(gray<group2){
+                red = 255;
+                green = 0;
+                blue = 0;
+                }
+             else if(gray<group3){
+                red = 173;
+                green = 216;
+                blue = 230;                
+                }
+             else{
+                red = 255;
+                green = 253;
+                blue = 208;
+                }
+             newColor = new Color(red, green, blue);
+             pixel.setColor(newColor);
+            }
+            volleyball.explore();
+            volleyball.write("images/sf2.jpg");
+          /**/
+         int group1 = (int) (255/5);
+         int group2 = (int) ((2*255)/5);
+         int group3 = (int) ((3*255)/5);
+         int group4 = (int) ((4*255)/5);
+         for (Pixel pixel: vPixels){
+             int gray = pixel.getRed();
+             Color newColor;
+             if (gray<group1){
+                // 143, 45, 86
+                // 0, 78, 100
+                newColor = new Color(0, 78, 100);
+                }
+             else if(gray<group2){
+                 // 216, 17, 89
+                 //0, 165, 207
+                newColor = new Color(0, 165, 207);
+                }
+             else if(gray<group3){
+                 // 255, 188, 66
+                 // 159, 255, 203
+                newColor = new Color(159, 255, 203);             
+                }
+             else if (gray<group4){
+                 // 33, 131, 128
+                 // 37, 161, 142
+                newColor = new Color(37, 161, 142);
+                }
+             else{
+                 // 115, 210, 222
+                 // 122, 229, 130
+                 newColor = new Color(122, 229, 130);
+                }
+             pixel.setColor(newColor);
+            }
+            volleyball.explore();
+            //volleyball.write("images/sf4.jpg");
 
          
     }//main       
